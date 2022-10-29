c4403815d60d84a0b5827d89619a8742deedf0a7
wsm95 <wsmcrae93@gmail.com>
2022-10-29 11:14:31 -0500

    en: sort scoreboard desc

--- src/database/scripts/currentScores.ts
+++ src/database/scripts/currentScores.ts
@@ -1,3 +1,3 @@
 export const currentScores_SQL = `
-  SELECT current_score, player_id FROM scores WHERE scores.game_id = $gameId ORDER BY scores.current_score;
+  SELECT current_score, player_id FROM scores WHERE scores.game_id = $gameId ORDER BY scores.current_score DESC;
 `;
